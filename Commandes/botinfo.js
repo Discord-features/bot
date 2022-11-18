54c9d681bc51cdae8f66d28cb48adcef9f707e0f
arayreth <contactrayreth@gmail.com>
2022-11-18 16:21:13 +0100

    Fix botinfo commands

--- Commandes/botinfo.js
+++ Commandes/botinfo.js
@@ -8,17 +8,15 @@ module.exports = new Command({
 	cooldown: 5, 
 
     async run(bot, message, args, db){
-     db.query(`SELECT * FROM server WHERE id_server = '${message.guild.id}'`, function (err, result) {
-            if (err) throw err;
-            console.log(result);
-                const language = result[0].languages;
-                if (language === "fr") {
-                 console.log("message en fr")   
-                 message.reply("Cette commande est en cours de développement, elle sera disponible dans les prochaines mises à jour.") 
-                }
-                else{
-                    console.log("message en en")
-                    message.reply("This command is under development, it will be available in the next updates.") 
-                }
-})
+  
+
+  db.query(`SELECT * FROM server WHERE id_server = ${message.guild.id}`, async (err, req) => {
+       const languages =  await req.languages;
+       if(languages === "fr"){
+        console.log("fr")
+       }
+       else if(languages === "en"){
+        console.log("en")
+       }
+    })
 }})
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # bot
 Discord features discord bot
+<h1>The first Discord bot of features beta in discord</h1> 
